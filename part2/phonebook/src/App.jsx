660fa40103fc17f51abbf2b858034982bf0083e2
Sergini34 <mendietasergio83gmail.com>
2026-02-27 14:23:02 -0400

    Usando useEffect para los contactos de phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 import Filter from './Components/Filter'
 import Formulario from './Components/Form'
 import Persons from './Components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        console.log(response)
+        setPersons(response.data)
+      })
+  },[])
+
+
   const [newName, setNewName] = useState('New Person')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState(persons)
--- practicas/practicas/src/App.jsx
+++ practicas/practicas/src/App.jsx
@@ -1,11 +1,24 @@
-import Note from "./components/Note"
-import { useState } from "react"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
   ) 
+  
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
   const [showAll, setShowAll] = useState(false)
   const noteShow = showAll ? notes : notes.filter(note => note.important === true)
 
